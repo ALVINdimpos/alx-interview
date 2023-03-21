40b6ec5e0eb517de91de4791e5cf6b00fecb5f92
ALVINdimpos <alvindimpos@gmail.com>
2023-03-21 10:07:38 +0200

    Add README with project description and instructions This commit adds a README file to the project, providing a brief overview of the python features covered in the repository, as well as instructions for getting started with the project and contributing to it.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,13 @@
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-    for i in range(1, n):
+    """
+    Generates Pascal's Triangle up to the nth row.
+    """
+    triangle = []
+    for i in range(n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+        if i > 0:
+            row.append(1)
         triangle.append(row)
-
-    return triangle
+    return triangle if n > 0 else []
