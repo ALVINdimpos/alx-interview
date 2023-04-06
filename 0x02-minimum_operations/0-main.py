a7c18d2b5e90221125b06897be03c432faba2927
ALVINdimpos <alvindimpos@gmail.com>
2023-04-06 15:01:45 +0200

    Added Minimum Operations functions

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-from minoperations import minOperations
+from 0-minoperations import minOperations
 
-if __name__ == '__main__':
-    n = 9
-    print(f"Min number of operations to reach {n} characters: {minOperations(n)}")
-    
-    n = 4
-    print(f"Min number of operations to reach {n} characters: {minOperations(n)}")
-    
-    n = 12
-    print(f"Min number of operations to reach {n} characters: {minOperations(n)}")
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
